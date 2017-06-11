9883f7b1950b23bf6def1d58f33271fd8ce8b630
Patrick Kubiak <patrick@antuple.net>
2017-06-11 00:33:32 -0400

    comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
+/*!
+ * RMC gulpfile
+ *
+ * @license
+ * Copyright (c) 2017 Rogers Manufacturing Company. All rights reserved.
+ */
+
+// include gulp & tools
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var fs = require('fs');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 
-var replace = require('gulp-replace');
-var fs = require('fs');
-
+// Compile Stylesheets
 gulp.task('sass', function () {
   return gulp.src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('css/'));
 });
 
+// Minify CSS
 gulp.task('clean-css', function () {
   return gulp.src('css/rmc-theme.css')
     .pipe(cleanCSS())
@@ -19,6 +28,7 @@ gulp.task('clean-css', function () {
     .pipe(gulp.dest('css/'));
 });
 
+// Inline CSS
 gulp.task('inline-css', function () {
   return gulp.src('build/es5-bundled/index.html')
     .pipe(replace('<link rel="stylesheet" href="css/rmc-theme.min.css">', function(s) {
@@ -28,5 +38,8 @@ gulp.task('inline-css', function () {
     .pipe(gulp.dest('build/es5-bundled/'));
 });
 
+// Compile Stylesheets
 gulp.task('css', ['sass', 'clean-css']);
+
+// Compile & Inline CSS
 gulp.task('default', ['css', 'inline-css']);
