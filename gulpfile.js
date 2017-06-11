c18c52d45d4fba988767d09ee8dc417c3e559240
Patrick Kubiak <patrick@antuple.net>
2017-06-11 00:14:02 -0400

    inline css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+
+var replace = require('gulp-replace');
+var fs = require('fs');
 
 gulp.task('sass', function () {
   return gulp.src('scss/**/*.scss')
@@ -16,7 +19,14 @@ gulp.task('clean-css', function () {
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('css', ['sass', 'clean-css']);
-gulp.task('default', ['css']);
+gulp.task('inline-css', function () {
+  return gulp.src('build/es5-bundled/index.html')
+    .pipe(replace('<link rel="stylesheet" href="css/rmc-theme.min.css">', function(s) {
+        var style = fs.readFileSync('css/rmc-theme.min.css', 'utf8');
+        return '<style>' + style + '</style>';
+    }))
+    .pipe(gulp.dest('build/es5-bundled/'));
+});
 
-// "css/rmc-theme.min.css",
+gulp.task('css', ['sass', 'clean-css']);
+gulp.task('default', ['css', 'inline-css']);
