49ee5f674f1dbb6758ae1bbef7b3dc349b81a667
Patrick Kubiak <patrick@antuple.net>
2017-06-29 16:08:07 -0400

    tool room data

---
+++ php/tool_room_equipment.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Tool Room Equipment list ordered by descending weight
+ * 
+ */
+
+// Database info
+require_once('db.php');
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$results = array();
+
+if ($db->connect_errno == 0) {
+    $myQuery = "SELECT * FROM `ToolRoomEquipment` ORDER BY `ToolRoomEquipment`.`count` DESC";
+    $result = $db->query($myQuery) or die($db->error);
+    
+    while ($row = $result->fetch_assoc()){
+        $results[] = $row;
+    }
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    echo json_encode($results);
+}
+?>
---
+++ php/tool_room_services.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Tool Room Services list in alphabetical order.
+ * 
+ */
+
+// Database info
+require_once('db.php');
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$results = array();
+
+if ($db->connect_errno == 0) {
+    $myQuery = "SELECT * FROM `ToolRoomServices` ORDER BY `ToolRoomServices`.`name` ASC";
+    $result = $db->query($myQuery) or die($db->error);
+    
+    while ($row = $result->fetch_assoc()){
+        $results[] = $row;
+    }
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    echo json_encode($results);
+}
+?>
