670b9287b8f5fc6785e3a5c60e3db04e0007be9d
Patrick Kubiak <patrick@antuple.net>
2017-06-29 15:07:24 -0400

    equipment list and manufacturing services sql tables

---
+++ php/equipment_list.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Equipment list ordered by descending weight
+ * 
+ */
+
+// Database info
+require_once('db.php');
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$results = array();
+
+if ($db->connect_errno == 0) {
+    $myQuery = "SELECT * FROM `EquipmentList` ORDER BY `EquipmentList`.`weight` DESC";
+    $result = $db->query($myQuery) or die($db->error);
+    
+    while ($row = $result->fetch_assoc()){
+        $results[] = $row;
+    }
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    echo json_encode($results);
+}
+?>
---
+++ php/manufacturing_services.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Manufacturing services in alphabetical order
+ * 
+ */
+
+// Database info
+require_once('db.php');
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$results = array();
+
+if ($db->connect_errno == 0) {
+    $myQuery = "SELECT * FROM `ManufacturingServices` ORDER BY `ManufacturingServices`.`name` ASC";
+    $result = $db->query($myQuery) or die($db->error);
+    
+    while ($row = $result->fetch_assoc()){
+        $results[] = $row;
+    }
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    echo json_encode($results);
+}
+?>
