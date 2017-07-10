0e7cea63b7731e73454037ca2cf9f0cd3d7620ef
Patrick Kubiak <patrick@antuple.net>
2017-07-10 11:51:38 -0400

    update tool room services

---
+++ php/tool_room_services_update.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Update Tool Room Equipment List
+ * 
+ */
+
+// Database info
+require_once('db.php');
+ 
+// Validate user input
+require_once('util/test_input.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if ($db->connect_errno == 0) {
+        //echo '<p>Connection to MySQL server successfully established.</p >';
+        
+        // validate & store input
+        $item = test_input($_POST['Item']);
+        
+        // update query
+        $UpdateQuery = sprintf("INSERT INTO `ToolRoomServices`(`name`) VALUES ('%s')",
+            intval($count), $db->real_escape_string($item));
+        
+        // execute query
+        $db->query($UpdateQuery) or die($db->error);
+        
+        // redirect user back to admin page
+        header("Location: https://therogersmanufacturingcompany.com/admin/");
+    }
+}
+?>
