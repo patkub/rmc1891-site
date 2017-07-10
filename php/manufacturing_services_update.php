e2bb3fc6905a4d7a1f7bf5805c9b223b6b348118
Patrick Kubiak <patrick@antuple.net>
2017-07-10 09:02:44 -0400

    update manufacturing services

---
+++ php/manufacturing_services_update.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Update Manufacturing Services
+ * 
+ */
+
+// Database info
+require_once('db.php');
+ 
+// Validate user input
+require_once('test_input.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if ($db->connect_errno == 0) {
+        //echo '<p>Connection to MySQL server successfully established.</p >';
+        
+        // validate & store input
+        $service = test_input($_POST['ManufacturingService']);
+        
+        // update query
+        $UpdateQuery = sprintf("INSERT INTO `ManufacturingServices`(`name`) VALUES (%s)",
+            $db->real_escape_string($service));
+        
+        // execute query
+        $db->query($UpdateQuery) or die($db->error);
+        
+        // redirect user back to admin page
+        header("Location: https://therogersmanufacturingcompany.com/admin/");
+    }
+}
+?>
