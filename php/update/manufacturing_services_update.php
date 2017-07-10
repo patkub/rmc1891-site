30145a8d6b158f19e1a0261b4c34a35c86bdf988
Patrick Kubiak <patrick@antuple.net>
2017-07-10 13:45:09 -0400

    php update file

--- php/update/manufacturing_services_update.php
+++ php/update/manufacturing_services_update.php
@@ -5,10 +5,10 @@
  */
 
 // Database info
-require_once('db.php');
+require_once('../db.php');
  
 // Validate user input
-require_once('util/test_input.php');
+require_once('../util/test_input.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
