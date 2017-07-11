cb7c7e14ef5141e12f1dd77fee4044e837382e78
Patrick Kubiak <patrick@antuple.net>
2017-07-11 09:57:34 -0400

    tool room equipment delete

---
+++ php/delete/tool_room_equipment_delete.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Delete from Tool Room Equipment List
+ *
+ * @author Patrick Kubiak
+ */
+
+// Database info
+require_once('../db.php');
+ 
+// Validate user input
+require_once('../util/test_input.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if ($db->connect_errno == 0) {
+        // successfully connected to MySQL server
+        
+        // validate & store input
+        $service = test_input($_POST['item']);
+        
+        // update query
+        $UpdateQuery = sprintf("DELETE FROM `ToolRoomEquipment` WHERE `name` = '%s'",
+            $db->real_escape_string($service));
+        
+        // execute query
+        $db->query($UpdateQuery) or die($db->error);
+    }
+}
+?>
