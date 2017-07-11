3dc12c40355f71b3e7623b7bcde3c18a324b5ca1
Patrick Kubiak <patrick@antuple.net>
2017-07-11 11:17:26 -0400

    machine => name

--- php/delete/equipment_list_delete.php
+++ php/delete/equipment_list_delete.php
@@ -20,11 +20,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // validate & store input
         $count = test_input($_POST['count']);
         $weight = test_input($_POST['weight']);
-        $machine = test_input($_POST['machine']);
+        $name = test_input($_POST['name']);
         
         // update query
         $UpdateQuery = sprintf("DELETE FROM `EquipmentList` WHERE `count` = %d AND `weight` = %d AND `name` = '%s'",
-            intval($count), intval($weight), $db->real_escape_string($machine));
+            intval($count), intval($weight), $db->real_escape_string($name));
         
         // execute query
         $db->query($UpdateQuery) or die($db->error);
