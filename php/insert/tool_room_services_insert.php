88f712e183f9521b1b7058e4bcd37da288c94410
Patrick Kubiak <patrick@antuple.net>
2017-07-11 09:43:35 -0400

    extend polymer gesture event listeners

--- php/insert/tool_room_services_insert.php
+++ php/insert/tool_room_services_insert.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // successfully connected to MySQL server
         
         // validate & store input
-        $service = test_input($_POST['service']);
+        $service = test_input($_POST['item']);
         
         // update query
         $UpdateQuery = sprintf("INSERT INTO `ToolRoomServices`(`name`) VALUES ('%s')",
