6ef6f0174a874f42b94a01e270f1223d224b692e
Patrick Kubiak <patrick@antuple.net>
2017-07-11 11:11:11 -0400

    equipment list delete

---
+++ php/delete/equipment_list_delete.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Delete from Equipment List
+ *
+ * @author Patrick Kubiak
+ */
+
+// Database info
+require_once('../db.php');
+ 
+// Validate user input
+require_once('../util/test_input.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if ($db->connect_errno == 0) {
+        // successfully connected to MySQL server
+        
+        // validate & store input
+        $count = test_input($_POST['count']);
+        $weight = test_input($_POST['weight']);
+        $machine = test_input($_POST['machine']);
+        
+        // update query
+        $UpdateQuery = sprintf("DELETE FROM `EquipmentList` WHERE `count` = %d AND `weight` = %d AND `name` = '%s'",
+            intval($count), intval($weight), $db->real_escape_string($machine));
+        
+        // execute query
+        $db->query($UpdateQuery) or die($db->error);
+    }
+}
+?>
