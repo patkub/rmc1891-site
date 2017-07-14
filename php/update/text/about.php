4a2805debf17054d5c08e95fe260cfe6cbbacf06
Patrick Kubiak <patrick@antuple.net>
2017-07-14 13:12:40 -0400

    check authentication for admin POST requests

--- php/update/text/about.php
+++ php/update/text/about.php
@@ -11,7 +11,7 @@ require_once('../../db.php');
 // Validate user input
 require_once('../../util/test_input.php');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SESSION['auth']) {
     $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
     if ($db->connect_errno == 0) {
