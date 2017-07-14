3c7a3ea406b62789408e527e0e9bbf740c9e5590
Patrick Kubiak <patrick@antuple.net>
2017-07-14 09:24:37 -0400

    db about text

---
+++ php/json/text/about.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * About text
+ * 
+ */
+
+// Database info
+require_once('../../db.php');
+
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$results = array();
+
+if ($db->connect_errno == 0) {
+    // successfully connected to MySQL server
+    
+    $myQuery = "SELECT `about` FROM `Text`";
+    $result = $db->query($myQuery) or die($db->error);
+    
+    while ($row = $result->fetch_assoc()){
+        $results[] = $row;
+    }
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    echo json_encode($results);
+}
+?>
