7966030cb937588c0b9b4d8e5135dc745d7c840e
Patrick Kubiak <patrick@antuple.net>
2017-07-17 08:05:50 -0400

    contact text view

--- sql/Text.sql
+++ sql/Text.sql
@@ -1,2 +1,3 @@
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('about', '');
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('capabilities', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('contact', '');
