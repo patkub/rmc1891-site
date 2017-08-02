33405a9f18cceecc6f64fce40e8c556a748043ba
Patrick Kubiak <patrick@antuple.net>
2017-08-02 08:36:49 -0400

    feature cards PUT route

--- api/index.php
+++ api/index.php
@@ -150,6 +150,37 @@ $app->get('/get/tool-room/services', function ($request, $response, $args) {
     return $response->withJson($results);
 });
 
+/**
+ * Feature cards PUT route.
+ * 
+ * @param  \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
+ * @param  \Psr\Http\Message\ResponseInterface      $res  PSR7 response
+ * @param  array                                    $args Route parameters
+ */
+$app->put('/put/feature-cards', function ($request, $response, $args) {
+    // Connect to MySQL database
+    $db = $this->get('myDb');
+    
+    // Get request body text
+    $body = $req->getParsedBody();
+    $aboutText = $body['AboutText'];
+    $capabilitiesText = $body['CapabilitiesText'];
+    $contactText = $body['ContactText'];
+    
+    // Format update queries
+    $aboutQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='About'",
+        $db->real_escape_string($aboutText));
+    $capabilitiesQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='Capabilities'",
+        $db->real_escape_string($capabilitiesText));
+    $contactQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='Contact'",
+        $db->real_escape_string($contactText));
+    
+    // Execute queries
+    $db->query($aboutQuery) or die($db->error);
+    $db->query($capabilitiesQuery) or die($db->error);
+    $db->query($contactQuery) or die($db->error);
+});
+
 // Run app
 $app->run();
 ?>
