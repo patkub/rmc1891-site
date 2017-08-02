7bfa966b6d35df80db84127c7610c77b1d4042d6
Patrick Kubiak <patrick@antuple.net>
2017-08-02 10:09:53 -0400

    rearranged routes

--- api/index.php
+++ api/index.php
@@ -47,6 +47,37 @@ $app->get('/get/text/{text}', function ($req, $res, $args) {
 });
 
 /**
+ * Text PUT route.
+ * 
+ * @param  \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
+ * @param  \Psr\Http\Message\ResponseInterface      $res  PSR7 response
+ * @param  array                                    $args Route parameters
+ * args['text'] array Defines the text to get, 'about', 'capabilities', or 'contact' text.
+ * 
+ * @return \Psr\Http\Message\ResponseInterface
+ */
+$app->put('/put/text/{text}', function ($req, $res, $args) {
+    // Check authentication
+    if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
+      // Unauthorized HTTP 401
+      return adminAuthError($res);
+    }
+    
+    // Connect to MySQL database
+    $db = $this->get('myDb');
+    
+    // Get request body text
+    $body = $req->getParsedBody();
+    
+    // Format update query
+    $updateQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='%s'",
+        $db->real_escape_string($body['text']), $db->real_escape_string($args['text']));
+    
+    // Execute query
+    $db->query($updateQuery) or die($db->error);
+});
+
+/**
  * Feature cards GET route.
  * 
  * @param  \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
@@ -65,6 +96,43 @@ $app->get('/get/feature-cards', function ($req, $res, $args) {
     // Return JSON
     header('Access-Control-Allow-Origin: *');
     return $res->withJson($results);
+});
+
+/**
+ * Feature cards PUT route.
+ * 
+ * @param  \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
+ * @param  \Psr\Http\Message\ResponseInterface      $res  PSR7 response
+ * @param  array                                    $args Route parameters
+ */
+$app->put('/put/feature-cards', function ($req, $res, $args) {
+    // Check authentication
+    if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
+      // Unauthorized HTTP 401
+      return adminAuthError($res);
+    }
+    
+    // Connect to MySQL database
+    $db = $this->get('myDb');
+    
+    // Get request body text
+    $body = $req->getParsedBody();
+    $aboutText = $body['AboutText'];
+    $capabilitiesText = $body['CapabilitiesText'];
+    $contactText = $body['ContactText'];
+    
+    // Format update queries
+    $aboutQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='About'",
+        $db->real_escape_string($aboutText));
+    $capabilitiesQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='Capabilities'",
+        $db->real_escape_string($capabilitiesText));
+    $contactQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='Contact'",
+        $db->real_escape_string($contactText));
+    
+    // Execute queries
+    $db->query($aboutQuery) or die($db->error);
+    $db->query($capabilitiesQuery) or die($db->error);
+    $db->query($contactQuery) or die($db->error);
 });
 
 /**
@@ -149,74 +217,6 @@ $app->get('/get/tool-room/services', function ($req, $res, $args) {
     // Return JSON
     header('Access-Control-Allow-Origin: *');
     return $res->withJson($results);
-});
-
-/**
- * Feature cards PUT route.
- * 
- * @param  \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
- * @param  \Psr\Http\Message\ResponseInterface      $res  PSR7 response
- * @param  array                                    $args Route parameters
- */
-$app->put('/put/feature-cards', function ($req, $res, $args) {
-    // Check authentication
-    if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
-      // Unauthorized HTTP 401
-      return adminAuthError($res);
-    }
-    
-    // Connect to MySQL database
-    $db = $this->get('myDb');
-    
-    // Get request body text
-    $body = $req->getParsedBody();
-    $aboutText = $body['AboutText'];
-    $capabilitiesText = $body['CapabilitiesText'];
-    $contactText = $body['ContactText'];
-    
-    // Format update queries
-    $aboutQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='About'",
-        $db->real_escape_string($aboutText));
-    $capabilitiesQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='Capabilities'",
-        $db->real_escape_string($capabilitiesText));
-    $contactQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='Contact'",
-        $db->real_escape_string($contactText));
-    
-    // Execute queries
-    $db->query($aboutQuery) or die($db->error);
-    $db->query($capabilitiesQuery) or die($db->error);
-    $db->query($contactQuery) or die($db->error);
-});
-
-/**
- * Text PUT route.
- * 
- * @param  \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
- * @param  \Psr\Http\Message\ResponseInterface      $res  PSR7 response
- * @param  array                                    $args Route parameters
- * args['text'] array Defines the text to get, 'about', 'capabilities', or 'contact' text.
- * 
- * @return \Psr\Http\Message\ResponseInterface
- */
-$app->put('/put/text/{text}', function ($req, $res, $args) {
-    // Check authentication
-    if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
-      // Unauthorized HTTP 401
-      return adminAuthError($res);
-    }
-    
-    // Connect to MySQL database
-    $db = $this->get('myDb');
-    
-    // Get request body text
-    $body = $req->getParsedBody();
-    
-    // Format update query
-    $updateQuery = sprintf("UPDATE FeatureCards SET text='%s' WHERE title='%s'",
-        $db->real_escape_string($body['text']), $db->real_escape_string($args['text']));
-    
-    // Execute query
-    $db->query($updateQuery) or die($db->error);
 });
 
 // Start php session
