42de9f673f6650207fa3a6fff68f72e1055defb9
Patrick Kubiak <patrick@antuple.net>
2017-08-02 13:21:46 -0400

    build to public_html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -26,6 +26,10 @@ import minimist from 'minimist';
 import browserSync from 'browser-sync';
 import historyFallback from 'connect-history-api-fallback';
 import pkg from './package.json';
+import poly from './polymer.json';
+
+// build path based on build name in polymer.json
+const BUILD_PATH = 'build/' + poly.builds[0].name + '/';
 
 const banner = ['<!--',
   '<%= pkg.name %> - <%= pkg.description %>',
@@ -89,17 +93,27 @@ gulp.task('clean-css', ['sass'], function() {
 });
 
 /**
+ * Copy api
+ */
+gulp.task('copy:api', function() {
+  return gulp.src([
+    'api/**/*',
+    'api/**/.*',
+  ]).pipe(gulp.dest('build/'));
+});
+
+/**
  * Copy fonts
  */
-gulp.task('fonts', function() {
+gulp.task('copy:fonts', function() {
   return gulp.src(FONTS)
-    .pipe(gulp.dest('build/es5-bundled/fonts/'));
+    .pipe(gulp.dest(BUILD_PATH + 'fonts/'));
 });
 
 /**
  * Copy fonts locally
  */
-gulp.task('fonts:local', function() {
+gulp.task('copy:fonts:local', function() {
   return gulp.src(FONTS)
     .pipe(gulp.dest('fonts/'));
 });
@@ -108,24 +122,22 @@ gulp.task('fonts:local', function() {
  * Inline CSS & banner
  */
 gulp.task('inline', function() {
-  return gulp.src('build/es5-bundled/index.html')
+  return gulp.src(BUILD_PATH + 'index.html')
     .pipe(replace('<link rel="stylesheet" href="css/rmc-theme.min.css">', function(s) {
       let style = fs.readFileSync('css/rmc-theme.min.css', 'utf8');
       return '<style>' + style + '</style>';
     }))
     .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest('build/es5-bundled/'));
+    .pipe(gulp.dest(BUILD_PATH));
 });
 
 /**
  * Generate precaching service worker
  */
-gulp.task('generate-service-worker', ['inline', 'fonts', 'del:after'], function(callback) {
-  let rootDir = 'build/es5-bundled/';
-  
-  swPrecache.write(path.join(rootDir, 'sw.js'), {
-    staticFileGlobs: [rootDir + '/**/*.{html,css,js,otf,eot,svg,ttf,woff,woff2,png,jpg,ico}'],
-    stripPrefix: rootDir,
+gulp.task('generate-service-worker', ['inline', 'copy:fonts', 'del:after'], function(callback) {
+  swPrecache.write(path.join(BUILD_PATH, 'sw.js'), {
+    staticFileGlobs: [BUILD_PATH + '/**/*.{html,css,js,otf,eot,svg,ttf,woff,woff2,png,jpg,ico}'],
+    stripPrefix: BUILD_PATH,
   }, callback);
 });
 
@@ -136,7 +148,7 @@ gulp.task('app-indexeddb', function() {
   return gulp.src([
     'node_modules/@npm-polymer/app-storage/app-indexeddb-mirror/app-indexeddb-mirror-worker.js',
     'node_modules/@npm-polymer/app-storage/app-indexeddb-mirror/common-worker-scope.js',
-  ]).pipe(gulp.dest('build/es5-bundled/'));
+  ]).pipe(gulp.dest(BUILD_PATH));
 });
 
 /**
@@ -153,7 +165,7 @@ gulp.task('del:before', function() {
  */
 gulp.task('del:after', function() {
   return del([
-    'build/es5-bundled/bower_components/',
+    BUILD_PATH + 'bower_components/',
   ]);
 });
 
@@ -182,12 +194,12 @@ gulp.task('serve:browsersync:local', () => {
 });
 
 /**
- * Serves production landing page from "build/es5-bundled/" directory.
+ * Serves production landing page from BUILD_PATH directory.
  */
 gulp.task('serve:browsersync:build', () => {
   browserSync({
     server: {
-      baseDir: 'build/es5-bundled/',
+      baseDir: BUILD_PATH,
       middleware: [
         historyFallback(),
       ],
@@ -197,9 +209,9 @@ gulp.task('serve:browsersync:build', () => {
 });
 
 /**
- * MySQL database connection info
+ * Database connection info
  */
-gulp.task('deploy:mysql', function() {
+gulp.task('deploy:db', function() {
   /* global process */
   let args = minimist(process.argv.slice());
   
@@ -209,7 +221,7 @@ gulp.task('deploy:mysql', function() {
     .pipe(replace('{{name}}', args.dbname))
     .pipe(replace('{{user}}', args.dbuser))
     .pipe(replace('{{pass}}', args.dbpass))
-    .pipe(gulp.dest('build/es5-bundled/private/'));
+    .pipe(gulp.dest(BUILD_PATH + 'private/'));
 });
 
 /**
@@ -221,10 +233,8 @@ gulp.task('deploy:public', function() {
   
   // public site
   return gulp.src([
-    'build/es5-bundled/**/*',
-    'build/es5-bundled/**/.*',
-    '!build/es5-bundled/private/*',
-    '!build/es5-bundled/private/.*',
+    BUILD_PATH + '**/*',
+    BUILD_PATH + '**/.*',
   ]).pipe(sftp({
     host: args.host,
     user: args.user,
@@ -242,8 +252,8 @@ gulp.task('deploy:private', function() {
   
   // private files
   return gulp.src([
-    'build/es5-bundled/private/**/*',
-    'build/es5-bundled/private/**/.*',
+    'private/**/*',
+    'private/**/.*',
   ])
     .pipe(replace('{{host}}', args.dbhost))
     .pipe(replace('{{name}}', args.dbname))
@@ -264,10 +274,10 @@ gulp.task('css', ['sass', 'clean-css']);
 gulp.task('build:before', ['del:before', 'css']);
 
 // After polymer build
-gulp.task('build:after', ['inline', 'fonts', 'del:after', 'generate-service-worker', 'app-indexeddb']);
+gulp.task('build:after', ['inline', 'copy:fonts', 'copy:api', 'del:after', 'generate-service-worker', 'app-indexeddb']);
 
 // Serve local
-gulp.task('serve:local', ['build:before', 'fonts:local', 'serve:browsersync:local']);
+gulp.task('serve:local', ['build:before', 'copy:fonts:local', 'serve:browsersync:local']);
 
 // Serve production
 gulp.task('serve:build', ['serve:browsersync:build']);
