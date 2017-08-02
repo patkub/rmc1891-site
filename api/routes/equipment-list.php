182efb230a75e2ee8ab62b89eee90cfd799174cb
Patrick Kubiak <patrick@antuple.net>
2017-08-02 11:21:14 -0400

    equipment list DELETE

--- api/routes/equipment-list.php
+++ api/routes/equipment-list.php
@@ -47,4 +47,32 @@ $app->put('/put/equipment-list', function ($req, $res, $args) {
     // Execute query
     $db->query($updateQuery) or die($db->error);
 });
+
+/**
+ * Equipment list DELETE route.
+ * 
+ * @param  \Psr\Http\Message\ServerRequestInterface $req  PSR7 request
+ * @param  \Psr\Http\Message\ResponseInterface      $res  PSR7 response
+ * @param  array                                    $args Route parameters
+ */
+$app->put('/delete/equipment-list', function ($req, $res, $args) {
+    // Check authentication
+    if (!isset($_SESSION['auth']) || $_SESSION['auth'] != true) {
+      // Unauthorized HTTP 401
+      return adminAuthError($res);
+    }
+    
+    // Connect to MySQL database
+    $db = $this->get('myDb');
+    
+    // Get request body
+    $body = $req->getParsedBody();
+    
+    // Format delete query
+    $deleteQuery = sprintf("DELETE FROM `EquipmentList` WHERE (`count` = %d AND `force` = %d AND `name` = '%s')",
+        intval($body['count']), intval($body['force']), $db->real_escape_string($body['name']));
+    
+    // Execute query
+    $db->query($deleteQuery) or die($db->error);
+});
 ?>
