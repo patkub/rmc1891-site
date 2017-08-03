166db362120ed1dbfa5f447ebe643eb721ebb249
Patrick Kubiak <patrick@antuple.net>
2017-08-03 08:07:44 -0400

    tabs

--- api/routes/login.php
+++ api/routes/login.php
@@ -13,35 +13,35 @@ $admin_user = "admin";
  * @return \Psr\Http\Message\ResponseInterface
  */
 $app->post('/post/login/', function ($req, $res, $args) {
-    // Connect to MySQL database
-    $db = $this->get('myDb');
+  // Connect to MySQL database
+  $db = $this->get('myDb');
 	
 	// Get admin password
-    $query = sprintf("SELECT `admin_pass` FROM `Admin` WHERE `admin_user` = '%s'",
-		$db->real_escape_string($admin_user));
+  $query = sprintf("SELECT `admin_pass` FROM `Admin` WHERE `admin_user` = '%s'",
+      $db->real_escape_string($admin_user));
 	$row = queryAndFetch($db, $query);
-    $admin_pass = $row["admin_pass"];
+  $admin_pass = $row["admin_pass"];
 	
 	// Get request body
-    $body = $req->getParsedBody();
+  $body = $req->getParsedBody();
 	
 	// Get attempted username and password
 	$user = $db->real_escape_string($body['username']);
 	$pass = $db->real_escape_string($body['password']);
 	
 	// Check username and password
-    if ($user === $admin_user && hash_equals($admin_pass, crypt($pass, $admin_pass))) {
-        // logged in successfully
-        $_SESSION['auth'] = true;
-        $response_array['status'] = 'success';
-    } else {
-        // login failed!
-        $_SESSION['auth'] = false;
-        $response_array['status'] = 'error';
-    }
-	
-    // Return JSON
-    header('Access-Control-Allow-Origin: *');
-    return $res->withJson($response_array);
+  if ($user === $admin_user && hash_equals($admin_pass, crypt($pass, $admin_pass))) {
+      // logged in successfully
+      $_SESSION['auth'] = true;
+      $response_array['status'] = 'success';
+  } else {
+      // login failed!
+      $_SESSION['auth'] = false;
+      $response_array['status'] = 'error';
+  }
+
+  // Return JSON
+  header('Access-Control-Allow-Origin: *');
+  return $res->withJson($response_array);
 });
 ?>
