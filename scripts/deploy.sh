12e1e6f07380487e3e95724d163fa786b803ca7a
Patrick Kubiak <patrick@antuple.net>
2017-08-10 08:49:38 -0400

    tabs => spaces

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 set -ev
 if [ "${CIRCLE_BRANCH}" = "deploy" ]; then
   gulp deploy:db --host "${FTP_HOST}" --user "${FTP_USER}" --pass "${FTP_PASS}" --dbhost "${DB_HOST}" --dbname "${DB_NAME}" --dbuser "${DB_USER}" --dbpass "${DB_PASS}"
-	gulp deploy:files --host "${FTP_HOST}" --user "${FTP_USER}" --pass "${FTP_PASS}"
+  gulp deploy:files --host "${FTP_HOST}" --user "${FTP_USER}" --pass "${FTP_PASS}"
 fi
