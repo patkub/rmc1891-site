7ea647e5ee9b47337cfae302eeb613e8af4529c5
Patrick Kubiak <patrick@antuple.net>
2017-12-26 16:30:45 -0500

    update text sql queries

--- sql/Text.sql
+++ sql/Text.sql
@@ -1,3 +1,7 @@
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('about', '');
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('capabilities', '');
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('contact', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('3d-scanning', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('3d-printing', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('mold-making', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('design-evaluation', '');
