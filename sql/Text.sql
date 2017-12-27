d5a4aa5cb3859e3f872e6c2911109f4c3fa3ba23
Patrick Kubiak <patrick@antuple.net>
2017-12-27 14:18:11 -0500

    update rapid prototyping headings

--- sql/Text.sql
+++ sql/Text.sql
@@ -1,7 +1,6 @@
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('about', '');
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('capabilities', '');
 INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('contact', '');
-INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('3d-scanning', '');
-INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('3d-printing', '');
-INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('mold-making', '');
-INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('design-evaluation', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('reverse-engineering', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('design-manufacture', '');
+INSERT INTO `db688129345`.`Text` (`name`, `text`) VALUES ('prototypes', '');
